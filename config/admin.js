6f25e6fc4eda5fe07b0843d13698b13dc8a25132
tareqsanabra <tareq.sanabra@gmail.com>
2020-03-23 22:59:11 +0100

    ws local port

--- config/admin.js
+++ config/admin.js
@@ -4,7 +4,6 @@ module.exports = {
 	language: process.env.LANGUAGE || 'en',
 	apiBaseUrl:
 		process.env.API_BASE_URL || 'https://api.demo.store.cyborgstech.com/api/v1',
-	apiWebSocketUrl:
-		process.env.API_WEB_SOCKET_URL || 'wss://api.demo.store.cyborgstech.com/ws',
+	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || 'ws://89.40.12.209:8020',
 	developerMode: process.env.DEVELOPER_MODE || true
 };
