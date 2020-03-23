8b341a50b078d639f0a376a2e3299e3af7db8e42
tareqsanabra <tareq.sanabra@gmail.com>
2020-03-23 21:09:22 +0100

    first

--- config/admin.js
+++ config/admin.js
@@ -2,7 +2,9 @@
 module.exports = {
 	// dashboard UI language
 	language: process.env.LANGUAGE || 'en',
-	apiBaseUrl: process.env.API_BASE_URL || 'http://localhost:3001/api/v1',
-	apiWebSocketUrl: process.env.API_WEB_SOCKET_URL || 'ws://localhost:3001',
+	apiBaseUrl:
+		process.env.API_BASE_URL || 'https://api.demo.store.cyborgstech.com/api/v1',
+	apiWebSocketUrl:
+		process.env.API_WEB_SOCKET_URL || 'wss://api.demo.store.cyborgstech.com',
 	developerMode: process.env.DEVELOPER_MODE || true
 };
